b9395b38a5616ef3122047bcbb1deec0bef5fd5d
Srujan A B <149394396+srujan2023@users.noreply.github.com>
2025-07-28 10:50:22 +0530

    Add files via upload

--- db.js
+++ db.js
@@ -1,15 +1,13 @@
-const mongoose = require("mongoose");
-const connectDB = async () => {
-    try{
-       const conn = await mongoose.connect(
-        "mongodb+srv://nodeapi:esT0V5CEthaU7zqt@nodeapi-cluster.7pvxd.mongodb.net/bookstore-api");
-       console.log(`MongoDB connected to ${conn.connection.host}`);
-       }
-    catch(error){
-     console.log(error);
-     process.exit(1);
-     
+const mongoose = require('mongoose')
+
+const connectDB = async () =>{
+    try {
+      const conn = await mongoose.connect(process.env.MONGO_URI)  
+      console.log(`MongoDB connected ${conn.connection.host}`);
+      
+    } catch (error) {
+        console.log(error);
+        process.exit(1)
     }
-    
-};
-module.exports = connectDB;
+}
+module.exports = connectDB
